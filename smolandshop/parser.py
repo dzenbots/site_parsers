59fd29508d6a7903a157e8377d146be6ee175236
vitaliy <vv.khritkin@gmail.com>
2024-03-23 11:39:17 +0300

    getting number of pages for every tabaco brand

--- smolandshop/parser.py
+++ smolandshop/parser.py
@@ -20,19 +20,43 @@ def get_html(url: str):
     return answer
 
 
+def process_brand(brand_name: str, brand_link: str):
+    html = get_html(brand_link)
+    parser = BeautifulSoup(html, "lxml")
+    try:
+        pages = parser.find("ul", {"class": "pagination paginator"})
+        last_page = pages.find_all("li")[-2]
+        try:
+            last_page_text = last_page.find("a").text
+            # print(last_page_text)
+            if last_page_text == "...":
+                last_page_number = int(last_page.find("a")["title"].split(" ")[-1])
+            else:
+                last_page_number = int(last_page_text)
+        except:
+            last_page_number = 0
+    except:
+        last_page_number = 0
+    print(last_page_number)
+
+
 def parse_url(url: str):
-    html = None
     try:
         html = get_html(url)
         with open('test.html', 'w') as file:
             file.write(html)
             parser = BeautifulSoup(html, "lxml")
-            rezult = parser.find("div", {"class": "shop-block-categories"}).find_all("div", {"class": "h2"})
-            for item in rezult:
-                inner = item.find("a")
-                print(f"{inner.text} --- {inner['href']}")
+            categories = parser.find("div", {"class": "shop-block-categories"}).find_all("div", {"class": "h2"})
+            for category in categories:
+                inner = category.find("a")
+                tabaco_brand_name = inner.text.rstrip(" для кальяна")[6:]
+                tabaco_brand_link = inner['href']
+                process_brand(brand_name=tabaco_brand_name, brand_link=tabaco_brand_link)
+                print(f"{tabaco_brand_name} --- {tabaco_brand_link}")
+                #
+
     except Exception as e:
-        ...
+        raise e
 
 
 if __name__ == "__main__":
