7704fffc7dd16bfc7bfa42c44f62e20b5ba8fe61
vitaliy <vv.khritkin@gmail.com>
2024-04-25 17:32:54 +0300

    fix is done

--- smolandshop/__init__.py
+++ smolandshop/__init__.py
@@ -1,6 +1,6 @@
-from .parser import parse_url, base_url
-
-__all__ = [
-    "parse_url",
-    "base_url"
-]
+# from .parser import parse_url, base_url
+#
+# __all__ = [
+#     "parse_url",
+#     "base_url"
+# ]
