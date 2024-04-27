45d82d940c9bf4064c466555a9339c4b7f13d58d
vitaliy <vv.khritkin@gmail.com>
2024-04-27 18:12:22 +0300

    fix is done

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import asyncio
 
-from smolandshop.smolandshop_parser import main
+from smolandshop.smolandshop_parser import main, finalize
 
 # initialize_db()
 # smolandshop.parse_url(smolandshop.base_url)
 # db.close()
 
-asyncio.run(main())
+# asyncio.run(main())
+asyncio.run(finalize())
